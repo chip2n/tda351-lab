dd2e53aa4accc8d84b4c18da62fb1920ea48b7eb
Andreas Arvidsson <andreas.arvidson@gmail.com>
2014-12-14 12:16:05 +0100

    Add some shit.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char
+import System.Random
 
 type Key = (Integer, Integer)
 type Signature = (Integer, Integer)
@@ -13,10 +14,11 @@ n = 160
 l :: Integer
 l = 1024
 
---sign :: DSAParameter -> Key -> Digest -> Signature
-sign (p,q,g) (x,y) msg = z
+sign :: DSAParameter -> Key -> Digest -> StdGen -> Integer
+sign (p,q,g) (x,y) msg gen = k
   where r = ((g^x) `mod` p) `mod` q
         z = convertS msg
+        (k, newGen) = random gen :: (Integer, StdGen)
         s = ()
 
 verify :: DSAParameter -> Key -> Digest -> Signature -> Bool
@@ -27,6 +29,16 @@ convertS :: String -> Integer
 convertS s = foldr (\(p,c) acc -> (+) acc . (*) (16^p). fromIntegral . digitToInt $ c) 0 s'
   where s' = zip [0..] (reverse s)
 
+extEuclidean :: Integer -> Integer -> (Integer, Integer, Integer)
+extEuclidean a b
+  | rem a b == 0 = (b, 0, 1)
+  | otherwise = undefined
+
 generateSecret :: DSAParameter -> Either String (Integer, Integer)
 generateSecret = undefined
+
+main :: IO ()
+main = do
+  gen <- newStdGen
+  print $ sign (7,7,3) (10,11) "abc" gen
 
